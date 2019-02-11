1e62348a9e51a7a4b747e12326579071f1132871
Gill Abada <gill@callifi.com>
2019-02-11 18:07:52 -0500

    add some functionality to buttons console.logs.

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,15 +1,37 @@
 import React from 'react';
 import './Footer.css';
 
+let total_comments = 0;
+
 const Footer = () => {
     return (
         <div className="footer">
-            <i className="fas fa-comment"></i>
-            <i className="fas fa-sync"></i>
-            <i className="fas fa-heart"></i>
-            <i className="fas fa-envelope"></i>
+            <i className="fas fa-comment" onClick={comment}>{total_comments}</i>
+            <i className="fas fa-sync" onClick={repost}></i>
+            <i className="fas fa-heart" onClick={heart}></i>
+            <i className="fas fa-envelope" onClick={email}></i>
         </div>
     );
+}
+
+
+const comment = () => {
+    total_comments += 1;
+    console.log(total_comments);
+    console.log('COMMENTED');
+    return total_comments;
+}
+
+const repost = () => {
+    console.log('REPOSTED');
+}
+
+const heart = () => {
+    console.log('HEARTED');
+}
+
+const email = () => {
+    console.log('EMAILED');
 }
 
 export default Footer;
