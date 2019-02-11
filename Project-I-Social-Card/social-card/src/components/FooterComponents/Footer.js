1662a142994e20ef9889561654e949b67ec337f9
Gill Abada <gill@callifi.com>
2019-02-11 17:31:36 -0500

    add footer icons.

---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <i className="fas fa-comment"></i>
+            <i className="fas fa-sync"></i>
+            <i className="fas fa-heart"></i>
+            <i className="fas fa-envelope"></i>
+        </div>
+    );
+}
+
+export default Footer;
