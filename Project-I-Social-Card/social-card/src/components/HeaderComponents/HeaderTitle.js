bb3d21c414ea53fe4923aee7ca3727fc245bd3f5
Gill Abada <gill@callifi.com>
2019-02-11 15:40:16 -0500

    build and import all the React components.

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+        <div class="header-title">
+            <p>
+                Lambda School <span>@LambdaSchool • 26 jan</span>
+            </p>
+        </div>
+    )
+}
+
+export default HeaderTitle;
